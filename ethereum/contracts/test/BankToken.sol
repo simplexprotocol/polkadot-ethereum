6d15516229b55f67fc6f00bd8908998398e7ac17
denalimarsh <denalimarsh@gmail.com>
2020-09-21 22:46:36 +0200

    update contract imports, move .env file

--- ethereum/contracts/test/BankToken.sol
+++ ethereum/contracts/test/BankToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.2;
 
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
 
